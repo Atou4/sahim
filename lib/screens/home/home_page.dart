838bdbdbb034b0bb32ff689c5467e5854a9a6e4f
alibeast8 <touaitiaaliali@gmail.com>
2022-04-06 15:24:55 +0100

    FINAL 3

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:sahim/models/project_model.dart';
 import 'package:sahim/screens/my_donation/checkout_screen.dart';
+import 'package:sahim/screens/my_donation/my_donation_screen.dart';
+import 'package:sahim/screens/my_donation/my_donations.dart';
 import 'package:sahim/screens/my_donation/tracking_donation_page.dart';
 import 'package:sahim/theme/colors.dart';
 import 'package:sahim/theme/sahim_icons.dart';
@@ -25,6 +27,17 @@ class HomePage extends StatelessWidget {
             padding: const EdgeInsets.all(20.0),
             child: Image.asset("assets/img/logo_tmp.png"),
           )),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(left:20.0),
+              child: IconButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>const  MyDonationScreen()));
+                },
+                icon: const Icon(SahimIcons.happiness__smile__kindness__love_1,color: SahimColors.orange,size: 40,),
+              ),
+            ),
+          ],
         ),
         body: SafeArea(
             child: ListView(children: <Widget>[
