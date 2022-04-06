4b9166e841a45c345849fa92a5f0aa904f1dff85
mahmoud taouti <59087120+mahmoudtaouti@users.noreply.github.com>
2022-04-06 13:28:35 +0200

    my donation screen

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:sahim/screens/my_donation/my_donation_screen.dart';
 import 'package:sahim/screens/my_donation/tracking_donation_page.dart';
 import 'package:sahim/theme/colors.dart';
 import 'package:sahim/theme/sahim_icons.dart';
 import 'package:sahim/screens/home/stats_widget.dart';
 import '../../widgets/project_card_widget.dart';
-
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -17,22 +17,17 @@ class HomePage extends StatelessWidget {
       textDirection: TextDirection.rtl,
       child: Scaffold(
         appBar: AppBar(
-          leading: FittedBox(child: Padding(
+          leading: FittedBox(
+              child: Padding(
             padding: const EdgeInsets.all(15.0),
             child: Image.asset("assets/img/logo_tmp.png"),
           )),
           actions: [
-            IconButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context)=>const TrackingDonationPage()));
-              },
-              icon: const Icon(SahimIcons.happiness__smile__kindness__love_1,color: SahimColors.orange,size: 40,),
-            )
+
           ],
         ),
         body: SafeArea(
-          child: ListView(
-            children: <Widget>[
+            child: ListView(children: <Widget>[
           Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -65,37 +60,62 @@ class HomePage extends StatelessWidget {
                 child: ListView(
                   scrollDirection: Axis.horizontal,
                   children: const [
-                    ProjectCardWidget(imgUrl: 'assets/img/people-sharing-some-dried-dates.jpg', projectName: 'إفطار الصائم', orgName: "جمعية الأمل",restToDonatePercentage: 70,),
-                    ProjectCardWidget(imgUrl: 'assets/img/old_man.jpeg',projectName:  'إعانة كبار السن',orgName: "جمعية دار الرحمة", restToDonatePercentage:51,),
-                    ProjectCardWidget(imgUrl: 'assets/img/homeless-people.jpg', projectName: 'مشروع إواء المحتاجين',orgName: "جمعية الأمل", restToDonatePercentage: 34,),
-                    ProjectCardWidget(imgUrl: 'assets/img/down-syndrome-painting.jpg', projectName: 'عناية أطفال متلازمة داون',orgName: "إنسان", restToDonatePercentage: 14,),
-                  ],
-                ),
-              ),
-
-             const Center(child:  Image(height: 100,width:300, image: AssetImage('assets/pics/ahsino_ayah.png'),)),
-
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("مجالات التبرع",
-                        style: Theme.of(context).textTheme.headline4),
-                    const Spacer(),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 6.0),
-                      child: Text(
-                        "عرض المزيد",
-                        style: Theme.of(context).textTheme.overline,
-                      ),
+                    ProjectCardWidget(
+                      imgUrl: 'assets/img/people-sharing-some-dried-dates.jpg',
+                      projectName: 'إفطار الصائم',
+                      orgName: "جمعية الأمل",
+                      restToDonatePercentage: 70,
+                    ),
+                    ProjectCardWidget(
+                      imgUrl: 'assets/img/old_man.jpeg',
+                      projectName: 'إعانة كبار السن',
+                      orgName: "جمعية دار الرحمة",
+                      restToDonatePercentage: 51,
+                    ),
+                    ProjectCardWidget(
+                      imgUrl: 'assets/img/homeless-people.jpg',
+                      projectName: 'مشروع إواء المحتاجين',
+                      orgName: "جمعية الأمل",
+                      restToDonatePercentage: 34,
+                    ),
+                    ProjectCardWidget(
+                      imgUrl: 'assets/img/down-syndrome-painting.jpg',
+                      projectName: 'عناية أطفال متلازمة داون',
+                      orgName: "إنسان",
+                      restToDonatePercentage: 14,
                     ),
                   ],
                 ),
               ),
-              SizedBox(
-                height: size.height * 0.03,
-              ),
+
+              const Center(
+                  child: Image(
+                height: 100,
+                width: 300,
+                image: AssetImage('assets/pics/ahsino_ayah.png'),
+              )),
+
+              // Padding(
+              //   padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              //   child: Row(
+              //     crossAxisAlignment: CrossAxisAlignment.start,
+              //     children: [
+              //       Text("مجالات التبرع",
+              //           style: Theme.of(context).textTheme.headline4),
+              //       const Spacer(),
+              //       Padding(
+              //         padding: const EdgeInsets.only(top: 6.0),
+              //         child: Text(
+              //           "عرض المزيد",
+              //           style: Theme.of(context).textTheme.overline,
+              //         ),
+              //       ),
+              //     ],
+              //   ),
+              // ),
+              // SizedBox(
+              //   height: size.height * 0.03,
+              // ),
 
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20.0),
@@ -115,47 +135,65 @@ class HomePage extends StatelessWidget {
                   ],
                 ),
               ),
-               Stack(
-                 children: [
-                   Positioned(
-                     bottom: -40,
-                     left: -20,
-                     child: Image.asset(
-                       "assets/pics/leaf_outlined.png",
-                       width: size.width * 0.35,
-                     ),
-                   ),
-                   Positioned(
-                     bottom: -50,
-                     right: -39,
-                     child: Image.asset(
-                       "assets/pics/leaf_solid.png",
-                       width: size.width * 0.5,
-                     ),
-                   ),
-                   Container(
-                     height: 400,
+              Stack(
+                children: [
+                  Positioned(
+                    bottom: -40,
+                    left: -20,
+                    child: Image.asset(
+                      "assets/pics/leaf_outlined.png",
+                      width: size.width * 0.35,
+                    ),
+                  ),
+                  Positioned(
+                    bottom: -50,
+                    right: -39,
+                    child: Image.asset(
+                      "assets/pics/leaf_solid.png",
+                      width: size.width * 0.5,
+                    ),
+                  ),
+                  Container(
+                    height: 400,
                     padding: const EdgeInsets.symmetric(horizontal: 20.0),
                     child: GridView.count(
-                      padding: const EdgeInsets.symmetric(horizontal: 20.0,vertical: 5),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20.0, vertical: 5),
                       crossAxisSpacing: 16,
                       mainAxisSpacing: 12,
                       physics: const NeverScrollableScrollPhysics(),
                       crossAxisCount: 2,
                       children: const [
-                        StatsCard(icon: SahimIcons.volunteer__charity__community__compassion, title: "عدد المستفيدين", count: 640,),
-                        StatsCard(icon: SahimIcons.donation__charity__volunteer__community, title: "إجمالي التبرعات", count: 1302300,),
-                        StatsCard(icon: SahimIcons.people__love__family__happy, title: "الجمعيات الناشطة", count: 73,),
-                        StatsCard(icon: SahimIcons.kindness__love__charity__give, title: "عدد التبرعات", count: 1240,)],
+                        StatsCard(
+                          icon: SahimIcons
+                              .volunteer__charity__community__compassion,
+                          title: "عدد المستفيدين",
+                          count: 640,
+                        ),
+                        StatsCard(
+                          icon: SahimIcons
+                              .donation__charity__volunteer__community,
+                          title: "إجمالي التبرعات",
+                          count: 1302300,
+                        ),
+                        StatsCard(
+                          icon: SahimIcons.people__love__family__happy,
+                          title: "الجمعيات الناشطة",
+                          count: 73,
+                        ),
+                        StatsCard(
+                          icon: SahimIcons.kindness__love__charity__give,
+                          title: "عدد التبرعات",
+                          count: 1240,
+                        )
+                      ],
                     ),
+                  ),
+                ],
               ),
-                 ],
-               ),
             ],
           ),
-            ]
-          )
-        ),
+        ])),
       ),
     );
   }
